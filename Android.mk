145b4fca3645a40ef4c84fdcaff747d5912a9439
DHOMD <DHO@dho.im>
2017-03-12 17:33:03 -0400

    Add a barrier for Marlin/Sailfish
    Change-Id: Ie72f9dee268cbae0e19de5c3054f4b8645cc50d3

--- Android.mk
+++ Android.mk
@@ -3,6 +3,7 @@
 # @brief Makefile for installing  time services header on Android.
 ################################################################################
 
+ifeq ($(filter marlin sailfish,$(TARGET_DEVICE)),)
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -11,3 +12,4 @@ LOCAL_COPY_HEADERS_TO := time-services
 LOCAL_COPY_HEADERS := ./time_genoff.h
 
 include $(BUILD_COPY_HEADERS)
+endif 
